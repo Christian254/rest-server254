0356140ea99f16a5a140a423d67b6b7dc64dc414
Christian254 <christianfuentes254@gmail.com>
2019-02-13 11:22:45 -0600

    CORS

--- server/rest-server.js
+++ server/rest-server.js
@@ -4,9 +4,16 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
+
 // parse application/json
 app.use(bodyParser.json())
 
