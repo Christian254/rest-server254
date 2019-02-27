0f6573fdbd61954ab6bdc0befd467c95a75fdff8
Christian254 <christianfuentes254@gmail.com>
2019-02-27 11:38:41 -0600

    Prueba

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-app.use(require('./usuario')); // llamando las rutas del usuario
+app.use(require('./usuario')); // llamand la rutas del usuario
 app.use(require('./login'));
 module.exports = app;
